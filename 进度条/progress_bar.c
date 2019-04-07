0c8fe23637405a20196c5e3ea26baa0cbdb3a7dc
cao <849807784@qq.com>
2019-04-07 04:29:26 -0400

    2019.04.07

---
+++ 进度条/progress_bar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main(){
+  int i = 0;
+  int j = 0;
+  for (i = 1; i <= 100; i++){
+    printf("[%d]",i);
+    for(j = 1; j <= i; j++){
+     printf("\e[1;%dm*\e[0m", 30 + i % 7);
+     fflush(stdout);
+    }
+
+    usleep(100000);
+    printf("\r");
+  }
+  printf("\n");
+  return 0;
+}
---
+++ 进度条/test.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+  int i = 0;
+  for (i = 0; i < 3; i++){
+    printf("haha\n");
+    sleep(1);
+  }
+  return 0;
+
+}
