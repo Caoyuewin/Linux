7d656b732ee9e6955a28641a3c326a0251d83395
cao <849807784@qq.com>
2019-04-07 05:22:15 -0400

    2019.04.07

--- 进度条/progress_bar.c
+++ 进度条/progress_bar.c
@@ -11,10 +11,26 @@ int main(){
      printf("\e[1;%dm*\e[0m", 30 + i % 7);
      fflush(stdout);
     }
-
     usleep(100000);
     printf("\r");
   }
   printf("\n");
   return 0;
 }
+/*
+int main(){
+  int i = 0;
+  int j = 0;
+  for (i = 1; i <= 100; i++){
+    printf("[%d]",i);
+    for(j = 1; j <= i; j++){
+     printf("\e[1;36m*\e[0m");
+     fflush(stdout);
+    }
+    usleep(100000);
+    printf("\r");
+  }
+  printf("\n");
+  return 0;
+}
+*/var
