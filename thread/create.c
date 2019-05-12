ae864c42eef5ef37125b2ca0d76512fd511e1dec
cao <849807784@qq.com>
2019-05-11 23:45:44 -0400

    2019.05.12

---
+++ thread/create.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+
+#if 0
+int main(int argc, char* argv[], char* env[]){
+  int i = 0;
+  while(env[i] != NULL){
+    printf("%s\n", env[i]);
+    i++;
+  }
+return 0;
+
+}
+#endif
+
+void* th_start(void* arg){
+  while(1){
+    printf("normal thread---%s\n",(char*)arg);
+    sleep(1);
+  }
+  return NULL;
+}
+
+
+int main(int argc, char* argv[]){
+ pthread_t tid;
+ int ret;
+ char* ptr = "play football";
+ ret = pthread_create(&tid, NULL, th_start, ptr);
+ if(ret){
+   perror("pthread_create error:");
+ }
+ while(1){
+   printf("mainthread id:%p]\n",tid);//tid:地址
+   sleep(1);
+ }
+  return 0;
+}
