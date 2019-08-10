27c4e645a40c3bc7203df30b09b0176b6d6b039c
Caoyuewin <849807784@qq.com>
2019-08-10 11:48:34 -0400

    API测试

--- order_system/server/dish_server.cc
+++ order_system/server/dish_server.cc
@@ -3,6 +3,29 @@
 #include "../db.hpp"
 
 MYSQL* mysql = NULL;
+//GetDishes
+bool GetDishes(const Json::Value& dish_ids, Json::Value* dishes, order_system::DishTable& dish_table) {
+        for(uint32_t i = 0; i < dish_ids.size(); ++i) {
+            int dish_id = dish_ids[i].asInt();
+            Json::Value dish_info;
+            bool ret = dish_table.SelectOne(dish_id, &dish_info);
+            if(!ret) {
+                printf("Select dish %d failed! \n", dish_id);
+                continue;
+            }
+            dishes->append(dish_info);
+        }
+        return true;
+    }
+
+
+int GetConsume(const Json::Value& dishes) {
+    int consume = 0;
+    for(uint32_t i = 0; i < dishes.size(); ++i) {
+        consume += dishes[i]["price"].asInt();
+    }
+    return consume;
+}
 
 int main() {
     using namespace httplib;
@@ -106,6 +129,8 @@ int main() {
             resp.set_content(writer.write(resp_json), "application/json");
             return ;
         }
+        //补上dish_id
+        resp_json["dish_id"] = dish_id;
         if(req_json["dish_id"].empty() || req_json["price"].empty()) {
             resp_json["ok]"] = false;
             resp_json["reason"] = "Request has no name  or price!\n";
@@ -147,9 +172,9 @@ int main() {
             resp.set_content(writer.write(resp_json), "application/json");
             return ;
         }
-        //转换接口，吧dishes转换成字符串，存到dishes_str中，数据库中插入dishes_str
+        //转换接口，吧dishes转换成字符串，存到dishes中，数据库中插入dishes
         const Json::Value& dish_ids = req_json["dishes"];
-        req_json["dishes_str"] = writer.write(dish_ids);
+        req_json["dishes"] = writer.write(dish_ids);
         ret = order_table.Insert(req_json);
         if(!ret) {
             resp_json["ok]"] = false;
@@ -158,14 +183,76 @@ int main() {
             resp.set_content(writer.write(resp_json), "application/json");
             return ;
         }
+        resp_json["ok"] = true;
+        resp.set_content(writer.write(resp_json), "application/json");
+        return;
     });
     //修改订单状态
     server.Put(R"(/order/(\d+))", [&order_table](const Request& req, Response& resp) {
+        Json::Reader reader;
+        Json::FastWriter writer;
+        Json::Value req_json;
+        Json::Value resp_json;
+        
+        int order_id = std::stoi(req.matches[1]);
 
+        bool ret = reader.parse(req.body, req_json);
+        if(!ret) {
+            resp_json["ok]"] = false;
+            resp_json["reason"] = "Parse failed!\n";
+            resp.status = 400;
+            resp.set_content(writer.write(resp_json), "application/json");
+            return ;
+        }
+        //补充order_id
+        req_json["order_id"]  = order_id;
+        if(req_json["order_id"].empty() || req_json["state"].empty()) {
+            resp_json["ok]"] = false;
+            resp_json["reason"] = "Request has no order_id or state!\n";
+            resp.status = 400;
+            resp.set_content(writer.write(resp_json), "application/json");
+            return ;
+        }
+        ret = order_table.ChangeState(req_json);
+        if(!ret) {
+            resp_json["ok]"] = false;
+            resp_json["reason"] = "ChangeState failed failed!\n";
+            resp.status = 500;
+            resp.set_content(writer.write(resp_json), "application/json");
+            return ;
+        }
+        resp_json["ok"] = true;
+        resp.set_content(writer.write(resp_json), "application/json");
+        return;
     });
     //获取订单
-    server.Get("(/order/)", [&order_table](const Request& req, Response& resp) {
+    server.Get("/order", [&order_table,&dish_table](const Request& req, Response& resp) {
+        Json::Reader reader;
+        Json::FastWriter writer;
+        Json::Value resp_json;
+        //查看菜品没有请求参数
+        Json::Value orders;
+        bool ret = order_table.SelectAll(&orders);
+        if(!ret) {
+            resp_json["ok"] = false;
+            resp_json["reason"] = "SelectAll failed";
+            resp.status = 500;
+            resp.set_content(writer.write(resp_json), "application/json");
+        }
+        for(uint32_t order_index = 0; order_index < orders.size(); ++order_index) {
+            Json::Value& order = orders[order_index];
+            Json::Value dish_ids;
+            ret = reader.parse(order["dishes"].asString(), dish_ids);
+            if(!ret) {
+                printf("No.%d parsefailed\n", order_index);
+                continue;
+            }
+            GetDishes(dish_ids, &order["dishes"], dish_table);
+            order["consume"] = GetConsume(order["dishes"]);
 
+        }
+        resp.set_content(writer.write(orders), "application/json");
+        return;
     });
     //获取用户客户端，匹配桌子id
     server.Get(R"(/client/table/(\S+))", [](const Request& req, Response& resp) {
